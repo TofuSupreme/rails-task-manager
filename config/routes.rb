d159c732d5a903f6232c17ab273d7f64e826415e
tofusupreme <yunishbacon@gmail.com>
2022-01-08 13:22:15 +0900

    added paths, show method, and index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get the list of tasks
+  get "/tasks/", to: "tasks#index"
+  # view details of a task
+  get "/task/:id", to: "tasks#show", as: :task
+  # add a new task
+  root to: "tasks#index"
+
+  #edit a task
+
+  #remove a task
 end
