195256f1789fae9ee8b4f634590eaec81112af9a
tofusupreme <yunishbacon@gmail.com>
2022-01-08 14:48:26 +0900

    Added update and edits to app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,36 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    # completed = task_params[:completed] == "0" ? false : true
+    # @task.completed = completed
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
   # get the list of tasks
   get "/tasks/", to: "tasks#index"
   # view details of a task
+
   get "/task/:id", to: "tasks#show", as: :task
+
   # add a new task
-  root to: "tasks#index"
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
 
   #edit a task
+  get "/tasks/:id/edit", to:"tasks#edit", as: :edit_task
+  patch "/task/:id", to:"tasks#update"
 
   #remove a task
 end
